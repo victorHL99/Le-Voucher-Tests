e1c0789371b3ef6cb1fcc5a4e3392d54634afd67
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-16 18:25:34 -0300

    test: create test for return Voucher Already Exist

--- tests/unit/voucherService.spec.ts
+++ tests/unit/voucherService.spec.ts
@@ -1,9 +1,13 @@
 import { jest } from "@jest/globals";
 import voucherService from "services/voucherService";
 import voucherRepository from "repositories/voucherRepository";
-import { conflictError } from "utils/errorUtils";
+import * as errorUtils from "utils/errorUtils";
 
 describe("VoucherService unit tests suite ", () => {
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it("should return success case for creation of voucher", async () => {
 
@@ -25,5 +29,27 @@ describe("VoucherService unit tests suite ", () => {
 
     expect(voucherRepository.createVoucher).toBeCalledWith(code, discount);
 
+  });
+
+  it("should return ''Voucher already exist.' error for creation of voucher", async () => {
+
+    const code = "TEST123";
+    const discount = 10;
+    const voucher = {
+      id: 1,
+      code,
+      discount,
+      used: false
+    };
+
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockResolvedValueOnce(voucher);
+    const mockConflictError = jest.spyOn(errorUtils, "conflictError").mockImplementationOnce((): any => {
+      return new Error("Voucher already exist.");
+    });
+
+    await expect(voucherService.createVoucher(code, discount)).rejects.toThrowError("Voucher already exist.");
+    expect(mockConflictError).toHaveBeenCalledWith('Voucher already exist.');
+    expect(voucherRepository.getVoucherByCode).toHaveBeenCalledWith(code);
+    expect(voucherRepository.createVoucher).not.toHaveBeenCalled();
   });
 });
