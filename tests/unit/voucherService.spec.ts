58cb38b5d77222c795a66cb3483b52cda0574bf2
victorHL99 <victor.lessa@estudante.ufla.br>
2023-03-16 17:56:24 -0300

    test: create test for return success case

--- tests/unit/voucherService.spec.ts
+++ tests/unit/voucherService.spec.ts
@@ -1,1 +1,29 @@
 import { jest } from "@jest/globals";
+import voucherService from "services/voucherService";
+import voucherRepository from "repositories/voucherRepository";
+import { conflictError } from "utils/errorUtils";
+
+describe("VoucherService unit tests suite ", () => {
+
+  it("should return success case for creation of voucher", async () => {
+
+    const code = "TEST123";
+    const discount = 10;
+    const voucher = {
+      id: 1,
+      code,
+      discount,
+      used: false
+    };
+
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockResolvedValueOnce(null);
+    jest.spyOn(voucherRepository, "createVoucher").mockImplementationOnce((): any => {
+      return voucher;
+    });
+
+    await voucherService.createVoucher(code, discount);
+
+    expect(voucherRepository.createVoucher).toBeCalledWith(code, discount);
+
+  });
+});
